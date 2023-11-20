038f2824456d8fdb7b91a44dcf360916664a7d19
689coding <86118109+689photo@users.noreply.github.com>
2023-11-20 21:18:07 +0900

    [level 2] Title: 더 맵게, Time: 1761.32 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while scoville[0] < K:
+        
+        if len(scoville) < 2:
+            return -1
+        
+        else:
+            temp = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+            heapq.heappush(scoville, temp)
+            answer += 1
+        
+    return answer
