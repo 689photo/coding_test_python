f442057665b44483d95fb8f37bf2574b20af8fb4
689coding <86118109+689photo@users.noreply.github.com>
2024-07-30 23:36:50 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,5 @@
 def solution(n):
-    m = n
-    while True:
-        m = m + 1
-        if bin(m)[2:].count('1') == bin(n)[2:].count('1'):
-               break
-    return m
+    temp = n + 1
+    while str(bin(n)[2:]).count('1') != str(bin(temp)[2:]).count('1'):
+        temp += 1
+    return temp
