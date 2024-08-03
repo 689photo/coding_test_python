894abca25f79b8740d21776e19e1a1f5093f8222
689coding <86118109+689photo@users.noreply.github.com>
2024-08-03 13:39:00 +0900

    [level 2] Title: 카펫, Time: 0.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,15 +1,10 @@
 def solution(brown, yellow):
-    by = brown + yellow
+    tt = brown + yellow
     result = []
-    for i in range(1, int((by + 1)**0.5) + 1):
-        if by % i == 0:
-            result.append([by//i, i])
-            
-    print(result)
     
-    for i in result:
-        if i[1] <= 2:
-            continue
-        else:
-            if (i[0] - 2) * (i[1] - 2) == yellow:
-                return i
+    for i in range(3, int(tt ** 0.5) + 1):
+        if tt % i == 0 and brown == (i - 2) * 2 + (tt//i) * 2:
+            result.append(tt // i)
+            result.append(i)
+    
+    return result
