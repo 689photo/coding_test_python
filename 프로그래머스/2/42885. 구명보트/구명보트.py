3e225032965955e242ab8798463237e109d20f09
689coding <86118109+689photo@users.noreply.github.com>
2024-08-04 12:10:46 +0900

    [level 2] Title: 구명보트, Time: 8.84 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,12 @@
 def solution(people, limit):
-    
-    answer = 0
     people.sort()
     left, right = 0, len(people) - 1
+    result = 0
     
     while left <= right:
-        if people[left] + people[right] > limit:
-            answer += 1
-            right -= 1
-        else:
-            answer += 1
+        if people[left] + people[right] <= limit:
             left += 1
-            right -= 1
+        right -= 1
+        result += 1
         
-    return answer
+    return result
