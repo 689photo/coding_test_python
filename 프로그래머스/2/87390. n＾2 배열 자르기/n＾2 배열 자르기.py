ac8e4e553558413816fdd2c0d89bb5521a0d0a9b
689coding <86118109+689photo@users.noreply.github.com>
2024-08-12 16:56:43 +0900

    [level 2] Title: n^2 배열 자르기, Time: 53.36 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,34 +1,8 @@
 def solution(n, left, right):
-    
     result = []
-    
-    for i in range(left, right+1):
-        temp = divmod(i, n)
-        if temp[1] > temp[0]:
-            result.append(temp[1] + 1)
-        else:
-            result.append(temp[0] + 1)
+    for k in range(left, right + 1):
+        i = k // n
+        j = k % n
+        result.append(max(i, j) + 1)
     
     return result
-    
-    """
-    y행의 y까지는 y + 1
-    y행의 y부터는 x + 1        
-    
-    [1, 2, 3]
-    [2, 2, 3]
-    [3, 3, 3]
-    
-    00 01 02 03
-    1  2  3  4
-    
-    10 11 12 13
-    2  2  3  4
-    
-    20 21 22 23
-    3  3  3  4
-    
-    30 31 32 33
-    4  4  4  4    
-    """
-    
