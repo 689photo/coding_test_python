22e7292fabecd855c890380fc1f31701634c047e
689coding <86118109+689photo@users.noreply.github.com>
2024-08-15 16:21:18 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,12 +1,20 @@
+from math import ceil
+
 def solution(progresses, speeds):
+    progresses = [ceil((100 - a) / e) for a, e in zip(progresses, speeds)]
+    
+    date = progresses[0]
+    deploy = 0
     result = []
-    while len(progresses) > 0:
-        before = len(progresses)
-        for x, y in enumerate(progresses):
-            progresses[x] = y + speeds[x]
-        if progresses[0] >= 100:
-            while len(progresses) > 0 and progresses[0] >= 100:
-                progresses.pop(0)
-                speeds.pop(0)
-            result.append(before - len(progresses))
+    
+    for i in progresses:
+        if i - date <= 0:
+            deploy += 1
+        else:
+            result.append(deploy)
+            deploy = 1
+            date = i
+    
+    result.append(deploy)
+    
     return result
