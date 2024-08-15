543b6267952a297f4ac61c1125068c7e67b076bc
689coding <86118109+689photo@users.noreply.github.com>
2024-08-15 15:21:54 +0900

    [level 2] Title: [1차] 캐시, Time: 67.98 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,19 +1,18 @@
 def solution(cacheSize, cities):
-    result = []
+    cache = []
     answer = 0
-    cities = list(i.upper() for i in cities)
+    
     for i in cities:
-        if len(result) < cacheSize and i not in result:
-            result.append(i)
-            answer += 5            
-        elif len(result) > 0 and i not in result:
-            result.pop(0)
-            result.append(i)
+        i = i.lower()
+        if i not in cache:
             answer += 5
-        elif i not in result:
-            answer += 5
+            if len(cache) < cacheSize:
+                cache.append(i)
+            else:
+                cache.append(i)
+                cache.pop(0)
         else:
-            result.remove(i)
-            result.append(i)
             answer += 1
+            cache.append(cache.pop(cache.index(i)))
+    
     return answer
